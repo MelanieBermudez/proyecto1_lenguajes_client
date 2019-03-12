09c3360f6e32dacd27d98e2fc502aa91b7970aa3
dereck05 <33290776+dereck05@users.noreply.github.com>
2019-03-12 00:39:35 -0600

    Add files via upload
    De momento archivo viejo .c, no copiar

---
+++ Client.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main(){
+    int net_socket;
+    net_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int conection_status = connect(net_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    printf("%i\n", conection_status);
+
+    //char mensaje[200] = "Hola soy dereck";
+    //send(net_socket,mensaje,sizeof(mensaje),0);
+	
+    char server_response[256];
+
+    recv(net_socket, &server_response, sizeof(server_response), 0);
+
+
+    printf("The server sent %s\n", server_response);
+
+    close(net_socket);
+
+    return 0;
+}
