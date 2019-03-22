8f79b9edb784734d801a4f701dccba5cc79b86d9
dereck05 <33290776+dereck05@users.noreply.github.com>
2019-03-21 20:32:25 -0600

    Se quita el caracter - de la conversacion y se agrega color  al destinatario

--- Client.c
+++ Client.c
@@ -12,17 +12,22 @@
 #define MSG_LEN 256
 #define USERNAME_LEN 32
 #define IP_LEN 16
+#define GRN   "\x1B[32m"
+#define BLU   "\x1B[34m"
+#define RESET "\x1B[0m"
 
 // Globals
 int clientSocket;
 char username[USERNAME_LEN];
 
-void reciveMessages(int clientSocket) {
+void reciveMessages() {
     char recvBuffer[MSG_LEN];
     while (1) {
         int recvStatus = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
         if (recvStatus > 0) {
-            printf("\r%s\n", recvBuffer);
+            printf(GRN "%s\n" RESET, recvBuffer);
+            fflush(stdout);
+            memset(recvBuffer, 0, sizeof(recvBuffer));
         }
 
         else if (recvStatus < 0) {
@@ -31,11 +36,14 @@ void reciveMessages(int clientSocket) {
         }
     }
 }
-void sendMessages(int clientSocket) {
+void sendMessages() {
     char sendBuffer[MSG_LEN];
     while (1) {
-	printf("Input: ");
+
         while (fgets(sendBuffer, MSG_LEN, stdin) != NULL) {
+
+            char* nl = strchr(sendBuffer, '\n');
+            *nl = '\0';
 
             if (strlen(sendBuffer) == 0) {
                 continue;
@@ -52,7 +60,7 @@ void sendMessages(int clientSocket) {
         }
     }
 }
-void validInput(char *username){
+void validInput(){
     printf("Ingrese su nombre: ");
     if (fgets(username, USERNAME_LEN, stdin) != NULL) {
         char* c = strchr(username, '\n');
@@ -67,66 +75,87 @@ void validInput(char *username){
     
 }
 
-void createClient(int * clientSocket){
-    *clientSocket = socket(AF_INET , SOCK_STREAM , 0);
-    if (*clientSocket == -1) {
+void createClient(){
+    clientSocket = socket(AF_INET , SOCK_STREAM , 0);
+    if (clientSocket == -1) {
         printf("Error fatal al crear el socket del cliente");
         exit(-1);
     }
-    
-    
 }
 
-void initialize(struct sockaddr_in* serverInfo,struct sockaddr_in *clientInfo,int *serverAddrLen ,int *clientAddrLen){
-    memset(serverInfo, 0, *serverAddrLen);
-    memset(clientInfo, 0, *clientAddrLen);
+void initialize(struct sockaddr_in* serverInfo,struct sockaddr_in *clientInfo){
+    int serverAddrLen = sizeof(serverInfo);
+    int clientAddrLen = sizeof(clientInfo);
 
-    serverInfo->sin_addr.s_addr = inet_addr("10.0.2.15"); //TODO cambiar esto por el del .config
+    memset(serverInfo, 0, serverAddrLen);
+    memset(clientInfo, 0, clientAddrLen);
+
+    serverInfo->sin_addr.s_addr = inet_addr("192.168.2.181"); //TODO cambiar esto por el del .config
     serverInfo->sin_port = htons(9001);
     serverInfo->sin_family = AF_INET;
 }
 
-void connection(int clientSocket , struct sockaddr* serverInfo,int serverAddrLen, int connectStatus,char * username){
-    connectStatus = connect(clientSocket,serverInfo, serverAddrLen);
+void connection(struct sockaddr_in* serverInfo, struct sockaddr_in *clientInfo){
+    int serverAddrLen = sizeof(serverInfo);
+    int clientAddrLen = sizeof(clientInfo);
+
+    int connectStatus = connect(clientSocket, (struct sockaddr*)serverInfo, serverAddrLen);
+
     if (connectStatus == -1) {
         printf("Error falta al conectarse con el servidor\n");
         exit(-1);
     }
+
+    getsockname(clientSocket, (struct sockaddr*) clientInfo, (socklen_t*) &clientAddrLen);
+    printf("Se ha conectado al servidor: %s\n", inet_ntoa(serverInfo->sin_addr));
 
     //Send username to server
     send(clientSocket, username, USERNAME_LEN, 0);
 }
 
 int start(){
+
+    //Username input
+    validInput();
+
+    //Create client socket
+    createClient();
+
     //Initialize variables
     struct sockaddr_in serverInfo;
     struct sockaddr_in clientInfo;
+
     int serverAddrLen = sizeof(serverInfo);
     int clientAddrLen = sizeof(clientInfo);
-    int connectStatus;
-    
-    //Username input
-    validInput(username);
 
-    //Create client socket
-    createClient(&clientSocket);
+    memset(&serverInfo, 0, serverAddrLen);
+    memset(&clientInfo, 0, clientAddrLen);
 
-    //Initialize client socket
-    initialize(&serverInfo,&clientInfo,&serverAddrLen,&clientAddrLen);
+    serverInfo.sin_addr.s_addr = inet_addr("10.0.2.15"); //TODO cambiar esto por el del .config
+    serverInfo.sin_port = htons(9001);
+    serverInfo.sin_family = AF_INET;
 
+    int connectStatus = connect(clientSocket, (struct sockaddr*)&serverInfo, serverAddrLen);
 
-    // Connect to Server
-    connection(clientSocket,&serverInfo,serverAddrLen, connectStatus,username);
+    if (connectStatus == -1) {
+        printf("Error falta al conectarse con el servidor\n");
+        exit(-1);
+    }
+
+    printf("Se ha conectado al servidor!\n");
+
+    //Send username to server
+    send(clientSocket, username, USERNAME_LEN, 0);
     
     //Make a child process
     int pid = fork();
 
     if (pid == 0) {
-        sendMessages(clientSocket);
+        sendMessages();
     }
 
     else{
-        reciveMessages(clientSocket);
+        reciveMessages();
     }
     	
     close(clientSocket);
